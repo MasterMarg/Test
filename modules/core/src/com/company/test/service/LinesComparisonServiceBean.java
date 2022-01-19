89835deb009376b985dad39cdef8c569cda0667e
Сергей <sergey_svotin@mail.ru>
2022-01-19 16:16:31 +0400

    v 2.1

--- modules/core/src/com/company/test/service/LinesComparisonServiceBean.java
+++ modules/core/src/com/company/test/service/LinesComparisonServiceBean.java
@@ -1,20 +1,28 @@
 package com.company.test.service;
 
+import com.haulmont.cuba.core.global.Messages;
 import org.springframework.stereotype.Service;
+import javax.inject.Inject;
 import java.util.Arrays;
 
 @Service(LinesComparisonService.NAME)
 public class LinesComparisonServiceBean implements LinesComparisonService {
+    @Inject
+    private Messages messages;
+
     public String getResult(String string) {
         StringBuilder answer = new StringBuilder();
         String[][] data = getDataFromString(string);
+        if (data.length > 2 || data.length < 1)
+            throw new RuntimeException(messages.getMainMessage("exception"));
         Arrays.stream(data[1]).flatMap(o -> Arrays.stream(data[0]).filter(o::contains)).distinct().
                 sorted().forEach(o -> answer.append(o).append(" "));
-        return answer.toString().trim();
+        return answer.length() > 0 ? answer.toString().trim() : messages.getMainMessage("emptyAnswer");
     }
 
     /**
      * This method divides a solid string into an actual two-dimensional array of strings
+     *
      * @param string is a string input of a user made by rules in the description of data field
      * @return a {@code String} two-dimensional array
      */
--- modules/core/src/com/company/test/service/MagicSquareServiceBean.java
+++ modules/core/src/com/company/test/service/MagicSquareServiceBean.java
@@ -1,6 +1,8 @@
 package com.company.test.service;
 
+import com.haulmont.cuba.core.global.Messages;
 import org.springframework.stereotype.Service;
+import javax.inject.Inject;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,10 +10,13 @@ import java.util.stream.Collectors;
 
 @Service(MagicSquareService.NAME)
 public class MagicSquareServiceBean implements MagicSquareService {
+    @Inject
+    private Messages messages;
+
     public String getResult(String string) {
         BigInteger[][] data = getInputDataFromString(string);
         BigInteger cost = new BigInteger("-1");
-        StringBuilder result = new StringBuilder("Ближайший по стоимости магический квадрат:\n");
+        StringBuilder result = new StringBuilder(messages.getMainMessage("title")).append("\n");
         int[][] closestMagicCircle = new int[3][3];
         for (int[][] baseData : getNormalMagicSquares()) {
             BigInteger tempCost = getTransformationCost(data, baseData);
@@ -24,13 +29,14 @@ public class MagicSquareServiceBean implements MagicSquareService {
             Arrays.stream(o).forEach(o1 -> result.append(o1).append(" "));
             result.deleteCharAt(result.length() - 1).append("\n");
         });
-        return result.append("Стоимость: ").append(cost).toString();
+        return result.append(messages.getMainMessage("ending")).append(" ").append(cost).toString();
     }
 
     /**
      * This method divides a solid string representation of a two-dimensional array of numbers into an actual
      * two-dimensional array and turns all elements to {@link BigInteger} just in case the numbers
      * are too large for any other class of integer numbers
+     *
      * @param string is a string representation of a two-dimensional array of numbers
      * @return a {@code BigInteger} two-dimensional array
      */
@@ -43,6 +49,7 @@ public class MagicSquareServiceBean implements MagicSquareService {
 
     /**
      * This method calculates all possible normal magic squares or a 3x3 size
+     *
      * @return an {@code ArrayList} of a two-dimensional {@code int} arrays representing normal magic squares
      */
     private ArrayList<int[][]> getNormalMagicSquares() {
@@ -53,8 +60,9 @@ public class MagicSquareServiceBean implements MagicSquareService {
 
     /**
      * This method generates all possible sequences from the characters in a given base string
-     * @param numBase is a string base for generation
-     * @param numSeq is a {@link StringBuilder} to build sequences
+     *
+     * @param numBase    is a string base for generation
+     * @param numSeq     is a {@link StringBuilder} to build sequences
      * @param resultList is an {@link ArrayList} to store completed sequences
      */
     private void getAllPossibleSequences(String numBase, StringBuilder numSeq, ArrayList<String> resultList) {
@@ -74,20 +82,22 @@ public class MagicSquareServiceBean implements MagicSquareService {
     /**
      * This method chooses magic squares from all possible sequences and turns it into actual
      * {@link ArrayList} of two-dimensional {@code int} arrays
+     *
      * @param inputData an {@code ArrayList} of {@link String} representations of magic squares to validate
      * @return an {@code ArrayList} of two-dimensional {@code int} arrays representing true magic squares
      */
     private ArrayList<int[][]> validateMagicSquares(ArrayList<String> inputData) {
         return getMagicSequencesInNumbers(new ArrayList<>(inputData.stream().filter(o ->
                 validateRowsAndColumns(o, getSumFromStringByIndexes(o, 0, 4, 8))
-                        && validateDiagonals(o, getSumFromStringByIndexes(o, 0, 4, 8))).
+                        && validateDiagonal(o, getSumFromStringByIndexes(o, 0, 4, 8))).
                 collect(Collectors.toList())));
     }
 
     /**
      * This method validates rows and columns of magic square
+     *
      * @param string is a {@link String} representation of a magic square
-     * @param sum is an {@code int} sum of a magic square
+     * @param sum    is an {@code int} sum of a magic square
      * @return {@code true} if all row and columns have the same sum as given in parameter
      */
     private boolean validateRowsAndColumns(String string, int sum) {
@@ -102,20 +112,22 @@ public class MagicSquareServiceBean implements MagicSquareService {
 
     /**
      * This method validates diagonal of a magic square
+     *
      * @param string is a {@link String} representation of a magic square
-     * @param sum is an {@code int} sum of this magic square
+     * @param sum    is an {@code int} sum of this magic square
      * @return {@code true} if diagonal have the same sum as given in parameter
      */
-    private boolean validateDiagonals(String string, int sum) {
+    private boolean validateDiagonal(String string, int sum) {
         return getSumFromStringByIndexes(string, 2, 4, 6) == sum;
     }
 
     /**
      * This method calculates the sum of digits from the string, generating a probable sum of a magic square
+     *
      * @param numData is a {@link String} representation of a magic square
-     * @param index1 is an {@code int} index of a first digit in a given string
-     * @param index2 is an {@code int} index of a second digit in a given string
-     * @param index3 is an {@code int} index of a third digit in a given string
+     * @param index1  is an {@code int} index of a first digit in a given string
+     * @param index2  is an {@code int} index of a second digit in a given string
+     * @param index3  is an {@code int} index of a third digit in a given string
      * @return an {@code int} sum of three chosen digits
      */
     private int getSumFromStringByIndexes(String numData, int index1, int index2, int index3) {
@@ -127,6 +139,7 @@ public class MagicSquareServiceBean implements MagicSquareService {
     /**
      * This method turns a {@link String} representations of a true magic squares into  actual
      * two-dimensional {@code int} arrays of digits
+     *
      * @param validatedMagicSquares is an {@code ArrayList} of string representations of true magic squares
      * @return an {@code ArrayLIst} of two-dimensional {@code int} arrays of digits
      */
@@ -139,7 +152,8 @@ public class MagicSquareServiceBean implements MagicSquareService {
 
     /**
      * This method counts a transformation cost for a given square and chosen true magic square
-     * @param data is a {@link BigInteger} two-dimensional array representing given square
+     *
+     * @param data     is a {@link BigInteger} two-dimensional array representing given square
      * @param baseData is a {@code int} two-dimensional array representing chosen true magic square
      * @return a {@code BigInteger} transformation cost
      */
